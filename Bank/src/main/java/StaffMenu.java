821b18e41f009f2c7a7028ac70f313d7c1fbdb76
Delaram Janafrsa <93863612+heyxdelaram@users.noreply.github.com>
2022-06-12 11:38:45 +0430

    Update StaffMenu.java

--- Bank/src/main/java/StaffMenu.java
+++ Bank/src/main/java/StaffMenu.java
@@ -17,6 +17,7 @@ public class StaffMenu extends javax.swing.JDialog {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Staff Menu");
+        setMinimumSize(new java.awt.Dimension(400, 300));
         getContentPane().setLayout(new java.awt.GridLayout(3, 0));
 
         createAccountButton.setText("Create Account");
