b87f51f5231568753b6f39ce98ff6c5ddeb03753
mobinazmi <100028706+mobinazmi@users.noreply.github.com>
2022-06-16 13:54:48 +0430

    Add files via upload

--- Bank/src/main/java/Controller/CustomerC.java
+++ Bank/src/main/java/Controller/CustomerC.java
@@ -1,9 +1,10 @@
 package Controller;
 
+import Entity.AccountE;
+import Entity.CardE;
 import Entity.CustomerE;
-import GUI.CustomerLogin;
-import GUI.CustomerMenu;
-import GUI.MainMenu;
+import GUI.*;
+import Service.CardS;
 import Service.CustomerS;
 
 import java.awt.*;
@@ -13,7 +14,6 @@ public class CustomerC {
     public void login() throws Exception{
         //MainMenu mainMenu = new MainMenu();
         try (CustomerLogin customerLogin = new CustomerLogin()){
-
             CustomerE customerE=new CustomerE();
             customerE.setNationalCode(customerLogin.getnCode());
             customerE.setPassword(customerLogin.getPass());
@@ -22,13 +22,45 @@ public class CustomerC {
             CustomerS customerS=new CustomerS();
             customerS.login(customerE1);
             if (customerE.getPassword() == customerE1.getPassword()){
-                new StandardResponse(StatusResponse.SUCCESS);
+                System.out.println(new StandardResponse(StatusResponse.SUCCESS));
                 customerLogin.dispose();
                 MainMenu mm = new MainMenu();
                 CustomerMenu customerMenu = new CustomerMenu(mm, true);
                 customerMenu.setVisible(true);
             }else
                 new StandardResponse(StatusResponse.ERROR);
+        }
+    }
+    public void transferMoney() throws Exception{
+        try (TransferMoneyForm transferMoneyForm=new TransferMoneyForm()){
+            CardE cardE=new CardE();
+            cardE.setCardNumber(transferMoneyForm.getPayeeNumText().getText());
+            cardE.setExpirationDate(transferMoneyForm.getExpiryDateText().getText());
+            cardE.setCvv2(Integer.parseInt(transferMoneyForm.getCvv2Text().getText()));
+
+            CardE cardE1=new CardE();
+            CardS cardS=new CardS();
+            cardS.getCard(cardE1);
+            if (cardE.getCardNumber()==cardE1.getCardNumber()&&cardE.getCvv2()==cardE1.getCvv2()&&cardE.getExpirationDate()==cardE1.getExpirationDate()){
+                new StandardResponse(StatusResponse.SUCCESS);
+            }
+        }
+    }
+    public double showBalance() throws Exception{
+        try (CustomerLogin customerLogin=new CustomerLogin()){
+            CustomerE customerE=new CustomerE();
+            customerE.setNationalCode(customerLogin.getnCode());
+            AccountE accountE=new AccountE();
+            accountE.setId(customerE.getId());
+            double balance=accountE.getBalance();
+        return balance;
+        }
+    }
+
+    public void payDebt() throws Exception{
+        try (PayDebtForm payDebtForm=new PayDebtForm()){
+            AccountE accountE=new AccountE();
+            accountE.setAccountNumber(payDebtForm.getPayeeNumText().getText());
         }
     }
 }
